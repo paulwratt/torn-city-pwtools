0b1a36f6addc0ea10b65595219c9bbe7ec66d272
Paul Wratt <paul.wratt@gmail.com>
2018-02-27 20:50:48 +1300

    Create rapest.torn.user.js

---
+++ rapest.torn.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Torn ReAttack Pest
+// @namespace    paulwratt.TornCity
+// @version      0.1
+// @description  Allows add user to Friends/Black list after _mugging_ someone
+// @author       paulwratt [2027970]
+// @homepage     https://paulwratt.github.io/torn-city-pwtools/
+// @include      https://www.torn.com/loader.php?sid=attackLog&ID=*
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
