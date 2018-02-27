5eb9cd2c7c5c092a8479efa949a732bd983795ce
Paul Wratt <paul.wratt@gmail.com>
2018-02-27 21:23:42 +1300

    add includes, using Torn Stats as a build guide

--- rapest.torn.user.js
+++ rapest.torn.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
-// @name         Torn ReAttack Pest
-// @namespace    paulwratt.TornCity
-// @version      0.1
-// @description  Allows add user to Friends/Black list after _mugging_ someone
+// @name         Torn City ReAttack Pest
+// @namespace    paulwratt.tornCity
+// @version      0.01
+// @description  Allows add user to Friends or Black list after _mugging_ someone
 // @author       paulwratt [2027970]
 // @homepage     https://paulwratt.github.io/torn-city-pwtools/
 // @include      https://www.torn.com/loader.php?sid=attackLog&ID=*
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.2/moment.min.js
 // ==/UserScript==
 
 (function() {
