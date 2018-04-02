0f24312e7ba0be8b24a9c63941ec36105f671dbe
Paul Wratt <paul.wratt@gmail.com>
2018-04-02 21:02:57 +1200

    add population & version + timers

--- tc.torn.js
+++ tc.torn.js
@@ -1,28 +1,27 @@
 /*
 tc.torn.js - generic script insert for Torn City
-builds an object from current page data (that has side-panel)
-
-see: https://github.com/paulwratt/torn-city-pwtools/raw/master/tc.torn.js.md
-
+             builds an object from current page data (that has side-panel)
+see: https://github.com/paulwratt/torn-city-pwtools/master/tc.torn.js.md
 */
 
-var tc = { debug: false, long: false, t: 0,
-       p: { n: '', id: '0', d: 0, l: { l: 0, u: false }, p: 0, m: 0, s: {},  }, 
+var tc = { ver: 1, debug: false, long: false, scripts: false, timers: true, t: 0,
+       p: { n: '', id: '0', d: 0, l: { l: 0, u: false }, p: 0, m: 0,  }, 
        e: { c:0, m:0, t: '0:00' }, n: { c:0, m:0, t: '0:00' }, h: { c:0, m:0, t: '0:00' }, l: { c:0, m:0, t: '0:00' },
-       f: { id: '0', c: { c: 0, m : 0, t: '0:00' } }
+       f: { id: '0', c: { c: 0, m : 0, t: '0:00' } },
+       s: {}
      };
 
 var tornCity = null;
 
 function tc_LongForm() {
+  if ( tc.long == false || tornCity != null) return;
   tornCity = { time: 0, 
                player: { name: '', 
-                         id: '',
+                         id: 0,
                          money: 0,
-                         level:   { lvl: 0, up: false },
                          points: 0,
                          merits: 0,
-                         scripts: {},
+                         level:   { lvl: 0, up: false }
                        },
                energy:  { cur: 0, max: 0, time: '0:00' },
                nerve:   { cur: 0, max: 0, time: '0:00' },
@@ -30,7 +29,8 @@ function tc_LongForm() {
                life:    { cur: 0, max: 0, time: '0:00' },
                faction: { id: 0,
                           chain: { cur: 0, max: 0, time: '0:00' }
-                        }
+                        },
+               scripts: {}
              };
 }
 
@@ -105,17 +105,60 @@ var cTimer = { chainTimer:true };
 
 // pass in the target node, as well as the observer options
 
+function tc_watchValues(){
 // values
 tc_observer.observe( energyValue, eValue );
 tc_observer.observe( nerveValue,  nValue );
 tc_observer.observe( happyValue,  hValue );
 tc_observer.observe( lifeValue,   lValue );
 tc_observer.observe( chainValue,  cValue );
+}
+
+function tc_attachTimers(){
 // timers
 tc_observer.observe( energyTimer, eTimer );
 tc_observer.observe( nerveTimer,  nTimer );
 tc_observer.observe( happyTimer,  hTimer );
 tc_observer.observe( lifeTimer,   lTimer );
 tc_observer.observe( chainTimer,  cTimer );
+}
 
+function tc_detectScripts(){
+  //
+}
+
+function tc_Populate(){
+  tc.t = 0;
+  if ( tc.scripts == true ) tc_detectScripts();
+  if ( tc.long == true ) {
+    if ( tornCity == null ) tc_LongForm();
+    tornCity.time = tc.t;
+    tornCity.player.name   = tc.p.n;
+    tornCity.player.id     = tc.p.id;
+    tornCity.player.money  = tc.p.d;
+    tornCity.player.points = tc.p.p;
+    tornCity.player.merits = tc.p.m;
+    tornCity.player.level.lvl = tc.p.l.l;
+    tornCity.player.level.up  = tc.p.l.u;
+    tornCity.energy.cur   = tc.e.c;
+    tornCity.energy.max   = tc.e.m;
+    tornCity.energy.timer = tc.e.t;
+    tornCity.nerve.cur   = tc.n.c;
+    tornCity.nerve.max   = tc.n.m;
+    tornCity.nerve.timer = tc.n.t;
+    tornCity.happy.cur   = tc.h.c;
+    tornCity.happy.max   = tc.h.m;
+    tornCity.happy.timer = tc.h.t;
+    tornCity.life.cur   = tc.l.c;
+    tornCity.life.max   = tc.l.m;
+    tornCity.life.timer = tc.l.t;
+    tornCity.faction.id   = tc.f.id;
+    tornCity.faction.chain.cur   = tc.f.c.c;
+    tornCity.faction.chain.max   = tc.f.c.m;
+    tornCity.faction.chain.timer = tc.f.c.t;
+  }
+  tc_whatValues();
+}
+
+setTimeout('tc_Populate(); if ( tc.timers == true ) tc_attachTimers();',2000);
 // https://github.com/paulwratt/torn-city-pwtools/raw/master/tc.torn.js
