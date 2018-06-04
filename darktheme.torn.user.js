f5a58165f010e2b5feb0eaf5c0d1b76e9abd439f
Paul Wratt <paul.wratt@gmail.com>
2018-06-05 09:07:07 +1200

    v2.00: total reorganisation
    update new event mouseover, update info bar at top, fixup settings button items and mouse overs, tweak settings "head" text, add job tabs

--- darktheme.torn.user.js
+++ darktheme.torn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dark Theme for Torn
 // @namespace    paulwratt.torn
-// @version      1.24
+// @version      2.00
 // @description  10 lines of CSS and one darkened image, to appease Nobody[237547] and Chedburn[1]
 // @author       paulwratt [2027970]
 // @homepage     https://paulwratt.github.io/torn-city-pwtools/
@@ -19,19 +19,20 @@
     GM_addStyle('' +
       'body.d, body.r { background: #111 url(https://paulwratt.github.io/torn-city-pwtools/imgs/bg_regular_dark.jpg) top left repeat !important; }' +
       '.d .profile-container, .d .gym-container .gym-box, .d .menu-body, .d .cont-gray10, .d .cont-gray { background-color: #626262 !important; }' +
-      '.d .preferences-container #prefs-tab-menu .headers li { background: linear-gradient(to bottom,#dfdfdf 0,#828282 100%) !important; }' +
+      '.d .preferences-container #prefs-tab-menu .headers li { background: linear-gradient(to bottom,#dfdfdf 0,#525252 100%) !important; }' +
+      '.d .preferences-container #prefs-tab-menu .headers li:hover { background: linear-gradient(to bottom,#626262 0,#424242 100%) !important; }' +
       '.d .preferences-container #prefs-tab-menu .headers li.delimiter { background: #626262 none !important; }' +
-      '.d .info-msg-cont.grey, .d .info-msg-cont { background: linear-gradient(to bottom,#dfdfdf 0,#828282 100%) !important; }' +
-      '.d .users-list>li.expanded { background: #626262 !important; }' +
+      '.d .preferences-container #prefs-tab-menu .headers li.ui-state-active { background: linear-gradient(to bottom,#deecef 0,#c6d3d7 100%) !important; }' +
+      '.d .info-msg .msg { background: linear-gradient(to bottom,#dfdfdf 0,#525252 100%) !important; }' +
+      '.d .users-list>li.expanded { background: #828282 !important; }' +
       '.d .users-list>li { background: #626262 !important; }' +
       '.d .users-list>li:hover { background: #828282 !important; }' +
-      '.d .travel-agency-travelling .stage>div.fade-left, .d .travel-agency-travelling .stage>div.fade-right { display: none !important; }' +
       '.d .forums-thread-wrap .thread-list>li .post-delimiter { background: #626262 !important; }' +
       '.d .container-body { background: #626262 !important; }' +
+      '.d .new .container-body { background: #525252 !important; }' +
       '.d .container-body:hover { background: #828282 !important; }' +
       '.d .container-body-list>li.red .container-body { background: #525252 !important; }' +
       '.d .container-body-list>li.red .container-body:hover { background: #828282 !important; }' +
-      '.d .new .container-body { background: #525252 !important; }' +
       '.d .fm-list>li { background: #626262 !important; }' +
       '.d .fm-list>li.new { background: #525252 !important; }' +
       '.d .fm-list>li:hover { background: #828282 !important; }' +
@@ -44,10 +45,9 @@
 //    '.d .forums-thread-wrap .action-wrap>li.ban.disabled:hover, .d .customize-items-list>li>span, ' +
 //    '.d .content-wrapper.logged-out-sidebar .thread-list .action-wrap>li:hover, .d .stock-main-wrap .stock-list .item .ui-accordion-header-active, ' +
 //    '.d .stock-main-wrap .stock-list .item .stock-tabs>li.ui-tabs-active, .d .sortable-list .latest-messages .list-cont-bg>li.new ' +
-      '.d .white-grad { background: linear-gradient(to bottom,#626262 0,#525252 100%) !important; }' +
-// fixups
-      '.d .faction-main-wrap .title-toggle.active+.cont-toggle a { color: #57a1c5 !important; text-decoration: none !important; pointer: }' +
-      '.d .faction-main-wrap .title-toggle.active+.cont-toggle a:visited { color: #c57557 !important; text-decoration: none !important; }' +
+      '.d .white-grad, .d .white-grad-tabs>li { background: linear-gradient(to bottom,#626262 0,#525252 100%) !important; }' +
+      '.d .white-grad-tabs li.ui-tabs-active, .d .white-grad-tabs>li:hover { background: linear-gradient(to bottom,#828282 0,#626262 100%) !important; }' +
+// factions
       '.d .chain-attacks-title { background-color: #626262 !important; }' +
       '.d .chain-attacks-list { background-color: #525252 !important; }' +
       '.d .f-war-list.war-new>li { background-color: #525252 !important; }' +
@@ -57,18 +57,16 @@
       '.d .f-war-list.war-new .faction-names { background-color: #626262 !important; }' +
       '.d .f-war-list.war-new .faction-war .tab-menu-cont .members-list { background-color: #525252 !important; }' +
       '.d .f-war-list.war-new .faction-war .tab-menu-cont .members-cont:not(.profile-mode) .join:hover { background: #626262 !important; }' +
+      '.d .faction-controls-wrap  { background-color: #626262 !important; }' +
+      '.d .faction-controls-wrap .control-tabs>li.ui-state-disabled a:hover, .d .faction-controls-wrap .control-tabs>li.ui-state-disabled a { background-color: #525252 !important; }' +
+      '.d .faction-controls-wrap .control-tabs>li a:hover, .d .faction-controls-wrap .control-tabs>li.ui-tabs-active a { background-color: #828282 !important; }' +
       '.d .faction-crimes-wrap .crimes-list .item-wrap.active, .d .faction-crimes-wrap .crimes-list .item-wrap.active .details-wrap, ' +
       '.d .faction-crimes-wrap .begin-wrap .crimes-list .item-wrap.active, .d .faction-crimes-wrap .begin-wrap .crimes-list .item-wrap.active .plans-wrap' +
       ' { background-color: #525252 !important; }' +
       '.d .armoury-tabs .item-list>li.item-give-act, .d .armoury-tabs .item-list>li.item-loan-act, .d .armoury-tabs .item-list>li.item-retrieve-act, ' +
       '.d .armoury-tabs .item-list>li.item-info-act, .d .armoury-tabs .item-list>li.item-use-act ' +
       ' { background-color: #828282 !important; }' +
-      '.d .armoury-tabs .item-list .item-action .give.active, .d .armoury-tabs .item-list .item-action .loan.active, ' +
-      '.d .armoury-tabs .item-list .item-action .retrieve.active, .d .armoury-tabs .item-list .item-action .use.active' +
-      ' { color: #57a1c5 !important; }' +
-      '.d .faction-controls-wrap  { background-color: #626262 !important; }' +
-      '.d .faction-controls-wrap .control-tabs>li.ui-state-disabled a:hover, .d .faction-controls-wrap .control-tabs>li.ui-state-disabled a { background-color: #525252 !important; }' +
-      '.d .faction-controls-wrap .control-tabs>li a:hover, .d .faction-controls-wrap .control-tabs>li.ui-tabs-active a { background-color: #828282 !important; }' +
+// items
       '.d .items-wrap .items-footer { background-color: #525252 !important; }' +
       '.d .items-wrap .items-cont>li { background-color: #626262 !important; }' +
       '.d .items-wrap .items-cont>li:hover { background-color: #828282 !important; }' +
@@ -76,15 +74,15 @@
       '.d .items-wrap .items-cont>li.act { background-color: #828282 !important; }' +
       '.d .show-item-info .item-cont .item-wrap { background-color: #828282 !important; }' +
       '.d .info-cont li, .d .additional-info li { background-color: #828282 !important; }' +
-      '.d .info-wrap .t-delimiter { background: linear-gradient(to bottom,#828282 0,rgba(242,242,242,0) 100%) !important; }' +
-      '.d .info-wrap .b-delimiter { background: linear-gradient(to bottom,rgba(242,242,242,0) 0,#828282 100%) !important; }' +
       '.d .show-item-info .item-cont .info-msg { background-color: #828282 !important; }' +
       '.d .items-list>li.act { background-color: #828282 !important; }' +
+      '.d .travel-agency-market .item-wrap { background-color: #828282 !important; }' +
 //    '.d .action-log .log-list li.color-3 .message-wrap { background: #626262 !important; }' +
       '.d .action-log .log-list li .message-wrap { background: #626262 !important; }' +
       '.d .blacklist .user-info-blacklist-wrap li:hover { background: #828282 !important; }' +
       '.d .specials-cont .item:hover { background-color: #828282 !important; }' +
       '.d .specials-cont>li:hover { background-color: #828282 !important; }' +
+      '.d .specials-cont-wrap .specials-cont>li.act { background-color: #828282 !important; }' +
       '.d .module-desc .info li { color: rgb(242, 242, 242) !important; }' +
       '.d .points-market .users-point-sell>li:hover { background-color: #828282 !important; }' +
       '.d .properties-list>li { background-color: #525252 !important; }' +
@@ -92,7 +90,6 @@
       '.d .properties-list>li.act { background-color: #828282 !important; }' +
       '.d .quick-links-wrap .columns > li .rows > li:hover { background-color: #828282 !important; }' +
       '.d .quick-links-wrap .columns > li .rows > li.title { background-color: #525252 !important; }' +
-      '.d .specials-cont-wrap .specials-cont>li.act { background-color: #828282 !important; }' +
       '.d .chart-placeholder { background-color: #828282 !important; }' +
       '.d .manage-company .funds .funds-cont { background-color: #828282 !important; }' +
       '.d .sortable-list .info-cont-wrap .divider span { background: #626262 !important; }' +
@@ -108,12 +105,26 @@
       '#nav-forums>div:hover, #nav-hall_of_fame>div:hover, #nav-my_faction>div:hover, #nav-recruit_citizens>div:hover, #nav-community_events>div:hover ' +
       ' { background: #828282 !important; }'+
       '#nav-friends>div:hover, #nav-enemies>div:hover { background: #626262 !important; }'+
-// below are tweaks
+// delimiters
+      '.d .info-wrap .t-delimiter { background: linear-gradient(to bottom,#828282 0,rgba(242,242,242,0) 100%) !important; }' +
+      '.d .info-wrap .b-delimiter { background: linear-gradient(to bottom,rgba(242,242,242,0) 0,#828282 100%) !important; }' +
+      '.d .l-delimiter { background: linear-gradient(to right,#828282 0,rgba(242,242,242,0) 100%) !important; }' +
+      '.d .r-delimiter { background: linear-gradient(to right,rgba(242,242,242,0) 0,#828282 100%) !important; }' +
+      '.d .preferences-container .l-delimiter { background: linear-gradient(to right,#626262 0,rgba(242,242,242,0) 100%) !important; }' +
+      '.d .preferences-container .r-delimiter { background: linear-gradient(to right,rgba(242,242,242,0) 0,#626262 100%) !important; }' +
+      '.d .travel-agency-travelling .stage>div.fade-left, .d .travel-agency-travelling .stage>div.fade-right { display: none !important; }' +
+// tweaks (mostly text and links)
       '.d .forums-thread-wrap .thread-list .column-wrap .poster-wrap { background: #424242 !important; }' +
+      '.d .faction-main-wrap .title-toggle.active+.cont-toggle a { color: #57a1c5 !important; text-decoration: none !important; pointer: }' +
+      '.d .faction-main-wrap .title-toggle.active+.cont-toggle a:visited { color: #c57557 !important; text-decoration: none !important; }' +
+      '.d .f-war-list.war-new .faction-war .tab-menu-cont .members-list .level { color:#323232 !important; }'+
+      '.d .armoury-tabs .item-list .item-action .give.active, .d .armoury-tabs .item-list .item-action .loan.active, ' +
+      '.d .armoury-tabs .item-list .item-action .retrieve.active, .d .armoury-tabs .item-list .item-action .use.active' +
+      ' { color: #57a1c5 !important; }' +
       '.d a.user.name, .d a.user.faction, .d .t-blue-cont a, .d .t-blue a, .d .t-blue { color: #57a1c5 !important; }' +
       'div.profile-sign a { color: #57a1c5 !important; text-decoration: none !important; }' +
       'div.profile-sign a:visited { color: #c57557 !important; text-decoration: none !important; }' +
-      '.d .f-war-list.war-new .faction-war .tab-menu-cont .members-list .level { color:#323232 !important; }'+
+      '.d .preferences-container .head.t-gray-6.bold { color: #a2a2a2 !important; }' +
 // patches for TornStats
       'div.profile-container.basic-info.bottom-round>div>table>tbody>tr>td>table>tbody>tr>td>a { color: #57a1c5 !important; text-decoration: none !important; }' +
 // patches for DocTorn
